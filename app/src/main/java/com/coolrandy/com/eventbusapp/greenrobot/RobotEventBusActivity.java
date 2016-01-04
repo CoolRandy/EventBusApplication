37581d73d1513c457c93793d60291599b9dea175
coolrandy <lvhaojack@gmail.com>
2016-01-04 18:10:27 +0800

    提交一个测试demo

---
+++ app/src/main/java/com/coolrandy/com/eventbusapp/greenrobot/RobotEventBusActivity.java
@@ -0,0 +1,20 @@
+package com.coolrandy.com.eventbusapp.greenrobot;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.coolrandy.com.eventbusapp.R;
+
+/**
+ * Created by admin on 2015/12/31.
+ */
+public class RobotEventBusActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.robot_eb_activity);
+
+    }
+}
